3cec1832c1114b8b181171e40a67d28f605c2ced
Henry Smith <henry.smith@netlify.com>
2022-08-12 12:23:10 +0100

    Update next config

--- plugins/parallel-build/generateSites.js
+++ plugins/parallel-build/generateSites.js
@@ -26,17 +26,19 @@ export const generateSites = async (path, netlifyConfig) => {
             repo: "hs-netlify/parallel-build",
             private: false,
             branch: "main",
+            cmd: netlifyConfig.build.command,
+            dir: ".next",
             env: {
               PARALLEL_PAGE_FRAG: `${dir}`,
               PARALLEL_NETLIFY_API_KEY: token,
             },
           },
-          // build_settings: {
-          //   env: {
-          //     PARALLEL_PAGE_FRAG: `${dir}`,
-          //     PARALLEL_NETLIFY_API_KEY: token,
-          //   },
-          // },
+          build_settings: {
+            env: {
+              PARALLEL_PAGE_FRAG: `${dir}`,
+              PARALLEL_NETLIFY_API_KEY: token,
+            },
+          },
           processing_settings: {
             ignore_html_forms: true,
           },
