e9b3d888b61c2c4ca424adb0f61c3ba0e4f341fd
Henry Smith <henry.smith@netlify.com>
2022-08-12 09:52:20 +0100

    Rebuild with diff check

--- plugins/parallel-build/index.js
+++ plugins/parallel-build/index.js
@@ -2,10 +2,9 @@ import { generateSites } from "./generateSites.js";
 import { ignorePages } from "./ignorePages.js";
 import { checkDiff, setToml } from "./utils.js";
 
-const target = process.env.PARALLEL_PAGE_FRAG;
-const parallelBuilt = netlifyConfig.build.environment.PARALLEL_BUILT || false;
-
 export const onPreBuild = async function ({ netlifyConfig, utils }) {
+  const target = process.env.PARALLEL_PAGE_FRAG;
+  const parallelBuilt = netlifyConfig.build.environment.PARALLEL_BUILT || false;
   const { git, build } = utils;
 
   const path = "./pages";
