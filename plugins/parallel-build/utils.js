bb9049cad7a943b05a211407480350bfabb0314e
Henry Smith <henry.smith@netlify.com>
2022-08-15 18:00:45 +0100

    Set next.config

--- plugins/parallel-build/utils.js
+++ plugins/parallel-build/utils.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { NetlifyAPI } from "netlify";
-import nextConfig from "./next.config";
+import nextConfig from "../../../next.config";
 
 export const getDirectories = (path) => {
   return fs.readdirSync(path).filter((file) => {
